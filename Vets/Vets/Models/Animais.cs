4292477fa2cae34920b2b4dc10af1c11190722f5
lipesco <filipe.branco@gmail.com>
2017-04-04 12:34:10 +0100

    alteração

--- Vets/Vets/Models/Animais.cs
+++ Vets/Vets/Models/Animais.cs
@@ -7,5 +7,11 @@ namespace Vets.Models
 {
     public class Animais
     {
+        public int AnimaisID { get; set; }
+        public string NomeDoAnimal { get; set; }
+        public string Especie { get; set; }
+        public string Raca { get; set; }
+        // ... outros atributos
+
     }
 }
